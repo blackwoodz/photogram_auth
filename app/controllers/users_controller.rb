c90eee84b4f96e6caad118080c7d5f7e5536e089
Nish George <skarlath001@gmail.com>
2016-11-22 19:29:06 -0600

    Added quick-like to all photo pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,14 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
+    @like_ids = Hash.new
+    @user.photos.each do |photo|
+      if photo.fan_list.include?(current_user.username)
+        @like_ids[photo.id] = Like.where('user_id = ' + current_user.id.to_s + ' AND photo_id = ' + photo.id.to_s).take.id
+      end
+    end
+
   end
 
   def my_likes
@@ -12,5 +20,12 @@ class UsersController < ApplicationController
 
   def my_timeline
     @timeline_photos = current_user.timeline_photos.order("created_at DESC")
+
+    @like_ids = Hash.new
+    @timeline_photos.each do |photo|
+      if photo.fan_list.include?(current_user.username)
+        @like_ids[photo.id] = Like.where('user_id = ' + current_user.id.to_s + ' AND photo_id = ' + photo.id.to_s).take.id
+      end
+    end
   end
 end
